498a7346c6bf5c75c99fddae74ce47ed7fee72fa
Rainycitymcr <d.barrowcliff@outlook.com>
2022-11-23 17:13:27 +0000

    more changes

--- ifelse.js
+++ ifelse.js
@@ -23,23 +23,103 @@
 // }
 
 // ACTIVITY 4 ////////////////////
+// const pizzatop = "cheese"
 
-// ACTIVITY 5 ////////////////////
+// switch(pizzatop) {
+//     case "pepper":
+//     case "salt":
+//         console.log("rough")
+//         break
+//     case "pineapple":
+//     case "cheese":
+//         console.log("fine this will do")
+//         break
+//     default:
+//         console.log("where is my pizza")
+// }
+
+// ACTIVITY 5 -8 ////////////////////
+// let password = "hello23456";
+
+// if (password.length < "8") {
+//     console.log ("Password is too short");
+// }
+// else if (password.length >="8") {
+//     console.log ("Log password");
+// }
+///////////////////////// ACTIVITY 6
+
+// let num = (1661);
+// let reversed = num.reverse();
+// reversed[] = 4;
+// console.log (numbers[1661]);
+
+let num = "1662"
+// if (num == num.reverse()) {
+//     console.log ("well done")
+// }
+// array1 = Array.from(num)
+// console.log(array1)
+// 
+// const newArray = num.split(" ");
+// const rev = newArray.reverse()
+// console.log(rev)
+
+
+/////////////////ACTIVITY 7
+// let num = 8;
+
+// if (num % 3 === 0 && num % 5 === 0) {
+//     console.log ("yes");
+// }
+// else if (num % 5 === 0) {
+//     console.log ("buzz");
+// }
+// else if (num % 3 === 0) {
+//     console.log ("fizz");
+// }
+// else {
+//     console.log ("no");
+// }
 
 // activity 9 //////////////////////
-let rdm = "jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi";
-// console.log(rdm.lastIndexOf("a")) 
-// console.log (rdm.lastIndexOf("e"))
-// console.log(rdm.lastIndexOf("i")) 
-// console.log (rdm.lastIndexOf("o"))
-// console.log(rdm.lastIndexOf("u")) 
+// let rdm = "jrfndklhgfndjkjlkgperfijfhdknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhejkdsoiufghedjwshi";
+// console.log(rdm.length)
+// let a = rdm.lastIndexOf("a")
+// let e = rdm.lastIndexOf("e")
+// let i = rdm.lastIndexOf("i") 
+// let o = rdm.lastIndexOf("o")
+// let u = rdm.lastIndexOf("u")
+// console.log(`the last vowel in the string is ${Math.max(a, e, i, o, u)}`)
+
 // console.log (rdm.length);
 // console.log (rdm.charAt(""))
 // console.log(Math.max(rdm.lastIndexOf("a"), rdm.lastIndexOf("e"),rdm.lastIndexOf("i"),rdm.lastIndexOf("o"),rdm.lastIndexOf("u")))
 
 // let last = rdm.charAt(rdm.length -1)
-// if (last = "e" || "a" || "i" || "o" || "u"){
+// if (last = "e" || "a" || "o" || "u"){
 //     console.log (`you have found a vowel and is at ${last.indexOf()}`)
 // }
-let last = rdm.lastIndexOf(["a"],["e"],["i"],["o"],["u"])
-console.log(last);
+// let last = rdm.lastIndexOf(["a"],["e"],["i"],["o"],["u"])
+// console.log(last);
+
+// for (let i = 0; i < rdm.length; i++) {
+//     console.log(rdm.lastIndexOf(["a","e","o","i","u"]))
+// }
+
+//////////////////////// ACTIVITY 10
+
+// const word = "abbt"
+// if (word.lastIndexOf() == word.charAt(0)) {
+//     console.log ("they are the same")
+// } else {
+//     console.log("try again!")
+// }
+// // also can do this!!/////////////////
+
+// if (word.charAt(word.length -1) == word.charAt(0)) {
+//     console.log("well done")
+// }
+// else {
+//     console.log("go away")
+// }
