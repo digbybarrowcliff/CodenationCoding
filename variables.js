1007089ef5820c9152126f51b6f37028eec5f2f2
Rainycitymcr <d.barrowcliff@outlook.com>
2022-11-23 11:24:01 +0000

    chasnges

---
+++ variables.js
@@ -0,0 +1,9 @@
+let favouriteDrink = "wine"
+let thisNumber = 5
+let firstName = "digby"
+console.log(favouriteDrink, thisNumber, firstName)
+console.log(`my name is ${firstName}, i am ${thisNumber} years old and I like ${favouriteDrink}`)
+
+// ACTIVITY 4 /////////////////////////////////
+const start = Date.now ()
+const birthday = new Date('1998-05-10')
