e20adc23b0d7d84eb5247a297ba33ac8e6d87ae9
Rainycitymcr <d.barrowcliff@outlook.com>
2022-11-23 15:10:52 +0000

    challenges updated

--- objects.js
+++ objects.js
@@ -1,3 +1,49 @@
+//////////////////// ACTIVITIES
+const pet = {
+    petName: "digby", 
+    typeOfPet: "dog",
+    age: 20,
+    colour: "red",
+    petFeatures: ["annyoying", "angry", "arrogant"],
+    eatdrink () {
+        if (this.age == 20 && this.petName == "digby"){
+            return `${this.petName} is drinking tequila`
+        }
+        else {
+            return "nothing"
+        }
+    }
+}
+console.log(pet.eatdrink())
+// console.log(`my ${pet.typeOfPet} is ${pet.age} years old and is called ${pet.petName}`)
+
+
+//////////////////////////////////////////////
+// const pet = {
+//     petName: "lizard",
+//     typeOfPet: "lizard",
+//     age: 3,
+//     colour: "brown",
+//     hobbies: ["skiing", "listening to early french revolutionary songs"],
+//     petFeatures: ["arrogant", "grating", "manipulative"],
+//     food: "biscuits",
+//     drink: "carver",
+//     petHome () {
+//         if (this.age >= 3 && this.food == "biscuits") {
+//             return `lizard is eating ${[pet.food]}`
+//         }
+//         else if (this.age <=2 && this.colour == "yellow") {
+//             return "lizard needs to stay"
+//         }
+//         else {
+//             return "what are we going to do"
+//         }
+//     }    
+// }
+// console.log (pet.petHome())
+
+///////////////////////////////////////ACTIVITY 5
+
 let totalCost;
 const coffeeShop = {
     branch: ["manchester", "Oldham"],
@@ -8,13 +54,14 @@ const coffeeShop = {
     // foodList: ["sandwich", "crisps", "apple"],
     // foodPrices: [5, 1, 0.5],
     foodOrder(f) {
-    console.log(`You ordered ${this.food[0][0]}. The price is ${this.food[0][1]} + ${this.food[1][1]} + ${this.food[2][1]}`)
+    return `You ordered ${this.food[0][0]}. The price is ${this.food[0][1]} + ${this.food[1][1]} + ${this.food[2][1]}`
     },
     drinkOrder(d) {
-    console.log(`You ordered ${this.drink[0]} and your price will be ${this.drink[0]}`)
+    return `You ordered ${this.drink[1][0]} and your price will be ${this.drink[1][1]}`
     }
 }
 console.log(coffeeShop.foodOrder())
+console.log(coffeeShop.drinkOrder())
 
 
 
--- variables.js
+++ variables.js
@@ -1,9 +1,35 @@
-let favouriteDrink = "wine"
-let thisNumber = 5
-let firstName = "digby"
-console.log(favouriteDrink, thisNumber, firstName)
-console.log(`my name is ${firstName}, i am ${thisNumber} years old and I like ${favouriteDrink}`)
+// let favouriteDrink = "wine"
+// let thisNumber = 5
+// let firstName = "digby"
+// console.log(favouriteDrink, thisNumber, firstName)
+// console.log(`my name is ${firstName}, i am ${thisNumber} years old and I like ${favouriteDrink}`)
 
-// ACTIVITY 4 /////////////////////////////////
-const start = Date.now ()
-const birthday = new Date('1998-05-10')
+// // ACTIVITY 4 ///////////////////////////////// calculating elapsed time
+
+const printElapsedTime = () => {
+    const startTime = performance.now(2022-11-23);
+    // const result = testFn();
+    const endTime = performance.now(2023-05-10);
+  
+    console.log(`Elapsed time: ${String(endTime - startTime)} milliseconds`);
+    return result;
+    printElapsedTime()
+  }
+  
+//   const yourFunctionReturn = printElapsedTime(yourFunction);
+
+
+
+
+
+
+//////////random code
+// petDeath () {
+//     if (this.health === 0)
+// }
+// if health === 0;
+// $().click(function(){
+//     $().trigger(death);
+// })
+
+// setTimeout (petDeath)
